4ee53890994c6e52da128a14988ccc1abd043851
kikijuju <114811246+kikijuju@users.noreply.github.com>
2026-01-05 23:41:25 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260105/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
+++ 260105/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.py
@@ -2,10 +2,15 @@ N = int(input())
 
 # Please write your code here.
 def fibo(n):
+    if n == 0:
+        return 0
     if n == 1:
         return 1
     if n == 2:
         return 2
-    return fibo(n-1) + fibo(n-2) 
-N -= 1    
-print(fibo(N))
+    
+    return fibo(n-1) + fibo(n-2)
+
+n = N-1
+
+print(fibo(n))
