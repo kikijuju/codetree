71314af73e13bc9f05fd85b38b8f0458cdd62970
kikijuju <114811246+kikijuju@users.noreply.github.com>
2026-02-27 17:39:56 +0900

    Title: k번째로 신기한 문자열, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260227/k번째로 신기한 문자열/kth-special-string.py
+++ 260227/k번째로 신기한 문자열/kth-special-string.py
@@ -3,18 +3,18 @@ n, k = int(n), int(k)
 str = [input() for _ in range(n)]
 
 str.sort()
-
+strr = []
 
 def is_it(i, t):
-    if t in i:
+    if i[:len(t)] == t:
         return True
     else:
         return False
 
 for i in str:
-    if not is_it(i, t):
-        str.remove(i)
-
+    if is_it(i, t):
+        strr.append(i)
+    else:
+        pass
      
-    
-print(str[k-1])
+print(strr[k-1])
