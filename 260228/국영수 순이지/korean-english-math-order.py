91ed5bab77e8c54229702f2cbe83987fbcb34a3e
kikijuju <114811246+kikijuju@users.noreply.github.com>
2026-02-28 17:39:35 +0900

    Title: 국영수 순이지, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260228/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+# Please write your code here.
+class Subject:
+    def __init__(self, name, korean, english, math):
+        self.name = name
+        self.korean = korean
+        self.english = english
+        self.math = math
+
+subs = []
+for _ in range(n):
+    name, korean, english, math = tuple(input().split())
+    subs.append(Subject(name, korean, english, math))
+
+subs.sort(key = lambda x : (x.korean, x.english, x.math), reverse = True)
+for i in range(n):
+    print(f'{subs[i].name} {subs[i].korean} {subs[i].english} {subs[i].math}')
